bda3ca63be7caaece61fde426eaf2a36ca0ffd68
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-09-30 06:07:56 -0700

    initial 3

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import * as core from '@actions/core';
 import { getPayload } from './getPayload';
 
-try {
-  core.debug('Initializing.');
+const main = async (): Promise<void> => {
+  try {
+    core.debug('Initializing.');
 
-  const str = await getPayload();
+    const str = await getPayload();
 
-  if (str) {
-    core.debug(str);
+    if (str) {
+      core.debug(str);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-} catch (error) {
-  core.setFailed(error.message);
-}
+};
+
+main();
