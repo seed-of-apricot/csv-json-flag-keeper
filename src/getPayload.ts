6f01731b06b9aab1618695f9a845708cb0c77c66
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-09-30 11:29:39 -0700

    initial 8

--- src/getPayload.ts
+++ src/getPayload.ts
@@ -1,9 +1,10 @@
 // import * as core from '@actions/core';
-// import * as github from '@actions/github';
+import * as github from '@actions/github';
 
 export const getPayload = async (): Promise<string | undefined> => {
   // const token = core.getInput('myToken');
   // const octokit = github.getOctokit(token);
+  console.log({ payload: github.context.payload });
 
   const { GITHUB_SHA } = process.env;
   return GITHUB_SHA;
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import { getPayload } from './getPayload';
 
 const main = async (): Promise<void> => {
   try {
-    // eslint-disable-next-line no-console
     console.log('Initializing.');
-
     const str = await getPayload();
 
     if (str) {
-      core.debug(str);
+      console.log(str);
     }
   } catch (error) {
     core.setFailed(error.message);
