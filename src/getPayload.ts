359040c00c00933b11b6f298a42830456406c862
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-09-30 05:59:08 -0700

    initial 2

--- src/getPayload.ts
+++ src/getPayload.ts
@@ -1,5 +1,5 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+// import * as core from '@actions/core';
+// import * as github from '@actions/github';
 
 export const getPayload = async (): Promise<string | undefined> => {
   // const token = core.getInput('myToken');
