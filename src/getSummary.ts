a933b4d346ce22bd39dfffe8ea5245dfe0d4920d
seed-of-apricot <seed.of.apricot@dev.chronolapselibrary.net>
2020-10-16 19:51:39 -0700

    0.5.2 1

--- src/getSummary.ts
+++ src/getSummary.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { OctokitResponse, ReposGetContentResponseData } from '@octokit/types';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { convertFiles } from './convertFiles';
 
 export const getSummary = async (): Promise<{
@@ -14,7 +13,7 @@ export const getSummary = async (): Promise<{
   const path = core.getInput('summaryPath');
   const file = await octokit.repos.getContent({
     ...github.context.repo,
-    ref: github.context.ref.replace('refs/heads/', ''),
+    ref: github.context.sha,
     path,
   });
 
